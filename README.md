13dc7495e66d1d420acab954d5532c596fe5d984
hahaharry10 <hnixon@hahaharry.dev>
2025-02-02 02:05:53 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Aim
+perma-hl provides a quick way to highlight segments of text, allowing easy navigation
+between screens while maintaining visual focus on important sections.
+
+# Inspiration
+In my studies into assembly code I constantly found myself reviewing two files side-
+by-side whilst documenting my learning on a third screen. As I switched focus between
+the two windows I frequently lost track of the segments I was looking at. I recently
+stumbled accross [folke/twilight.nvim](https://github.com/folke/twilight.nvim) which
+I thought was a really pretty and peaceful way to dim and focus segments of text. As
+a result I created this plugin as an unobtrusive way to focus my attention on
+specified areas of text.
+
+# Screenshot:
+
+
+# Installation:
+To install using Lazy:
+```
+```
---
+++ lua/focus/init.lua
@@ -0,0 +1,120 @@
+local M = {}
+
+-- TODO: Add feature allowing mulitple highlights on a page.
+
+-- Param: Table, 'coords':
+--      coords.end_row: the final row/line of the selected area,
+--      coords.end_col: the column the selected area ends on the final row.
+--      coords.start_row: the first row/line of the selected area.
+--      coords.start_col: the column the selected area starts on the first row.
+function M:highlight(coords)
+    coords = coords or {}
+
+    print(vim.inspect(coords))
+
+    if coords == {} then
+        return
+    end
+
+    if coords.end_row < coords.start_row or (coords.end_row == coords.start_row and coords.end_col < coords.start_col) then
+        -- swap line numbers
+        local buf = coords.end_row
+        coords.end_row = coords.start_row
+        coords.start_row = buf
+
+        -- swap column numbers
+        buf = coords.end_col
+        coords.end_col = coords.start_col
+        coords.start_col = buf
+    end
+
+    -- Ensure 1-based indexing:
+    coords.end_row = (coords.end_row > 0) and coords.end_row or 1
+    coords.end_col = (coords.end_col > 0) and coords.end_col or 1
+    coords.start_row = (coords.start_row > 0) and coords.start_row or 1
+    coords.start_col = (coords.start_col > 0) and coords.start_col or 1
+
+    local function getColour()
+        local str = vim.api.nvim_exec2("hi Comment", { output = true }).output
+        local start_index = string.find(str, "guifg=")
+        local end_index = string.find(str, " ", start_index)
+        return string.sub(str, start_index+string.len("guifg="), end_index)
+    end
+
+
+
+    local colour = getColour()
+
+    self.namespace_name = "perma_hl"
+    self.namespace_id = vim.api.nvim_create_namespace(self.namespace_name)
+
+    local hl_group = "perma_hl_group"
+    vim.api.nvim_set_hl(0, hl_group, { fg = colour, default = false })
+
+
+    vim.api.nvim_buf_clear_namespace(0, self.namespace_id, 0, -1)
+    for i = 1, vim.fn.line("$") do
+        if i < coords.start_row or i > coords.end_row then
+            vim.api.nvim_buf_add_highlight(
+                vim.api.nvim_get_current_buf(),
+                self.namespace_id,
+                hl_group,
+                i-1,
+                0,
+                -1
+            )
+        elseif i == coords.start_row and coords.start_col > 1 then
+            vim.api.nvim_buf_add_highlight(
+                vim.api.nvim_get_current_buf(),
+                self.namespace_id,
+                hl_group,
+                i-1,
+                0,
+                coords.start_col-1
+            )
+
+        elseif i == coords.end_row and coords.end_col < vim.fn.col({coords.end_row, "$"}) then
+            vim.api.nvim_buf_add_highlight(
+                vim.api.nvim_get_current_buf(),
+                self.namespace_id,
+                hl_group,
+                i-1,
+                coords.end_col,
+                -1
+            )
+        end
+    end
+end
+
+function M:unfocus()
+    vim.api.nvim_buf_clear_namespace(0, self.namespace_id, 0, -1)
+end
+
+function M:focus_visual_selection()
+    local start_pos = vim.fn.getcharpos(".")
+    local end_pos = vim.fn.getcharpos("v")
+
+    local coords = {}
+    coords.end_row = end_pos[2]
+    coords.end_col = end_pos[3]
+    coords.start_row = start_pos[2]
+    coords.start_col = start_pos[3]
+
+    local mode = vim.api.nvim_exec2("echo mode()", { output = true }).output
+
+    if mode == "V" then -- is mode Visual Line?
+        if coords.end_row > coords.start_row then
+            coords.end_col = vim.fn.col({coords.end_row, "$"})
+            coords.start_col = 0
+        else
+            coords.start_col = vim.fn.col({coords.start_row, "$"})
+            coords.end_col = 0
+        end
+    end
+
+
+    self:highlight(coords)
+end
+
+return M
+
